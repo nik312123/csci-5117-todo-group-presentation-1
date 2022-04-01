1117d1db6a8b85dacae72ef8fd36c4f76244294b
Nikunj Chawla <nikchawla312@gmail.com>
2022-03-31 20:54:30 -0500

    Set database dialect to Postgres and added schema file

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TYPE PRIORITY_T AS ENUM ('low', 'medium', 'high');
+
+CREATE TABLE usr (
+    email       VARCHAR(254) NOT NULL,
+    first_name  VARCHAR(30)  NOT NULL,
+    middle_name VARCHAR(30)  NULL,
+    last_name   VARCHAR(30)  NOT NULL,
+    profile_url VARCHAR(512) NULL
+);
+
+CREATE TABLE task (
+    id           SERIAL PRIMARY KEY,
+    title        VARCHAR(30)  NOT NULL,
+    description  VARCHAR(200) NOT NULL,
+    due_datetime TIMESTAMP    NOT NULL,
+    email        VARCHAR(254) NOT NULL,
+    color        CHAR(6)      NOT NULL,
+    priority     PRIORITY_T   NOT NULL,
+    is_completed BOOLEAN      NOT NULL
+);
