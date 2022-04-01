04064881d294b56cb27eab6a12141b29b6b3d0fb
Nikunj Chawla <nikchawla312@gmail.com>
2022-03-31 22:56:14 -0500

    Added initial Siyad files

---
+++ 1_siyad_final/task_model.py
@@ -0,0 +1,58 @@
+import re
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, validator, Field
+
+from user_model import UserModel
+
+
+class TaskModel(BaseModel):
+    id: int
+    title: str
+    description: str
+    due_datetime: datetime = Field(alias = "dueDatetime")
+    user: UserModel
+    color: str
+    priority: Literal["low", "medium", "high"]
+    is_completed: bool = Field(False, alias = "isCompleted")
+    
+    # Underscores prevent fields from being part of the schema parsed by pydantic
+    _title_format_regex = re.compile(r"^[a-zA-Z0-9 ]+$")
+    _color_format_regex = re.compile("^[a-zA-Z]{6}$")
+    
+    @validator("title", "description")
+    def strip_spaces(cls, string: str) -> str:
+        return string.strip()
+    
+    @validator("id")
+    def check_id_is_positive(cls, task_id: int) -> int:
+        if task_id <= 0:
+            raise ValueError("The id must be positive.")
+        return task_id
+    
+    @validator("title")
+    def check_title_length(cls, title: str) -> str:
+        title_len = len(title)
+        if title_len < 1 or title_len > 30:
+            raise ValueError("The title must be between 1 and 30 characters in length.")
+        return title
+    
+    @validator("title")
+    def check_title_format(cls, title: str) -> str:
+        if not cls._title_format_regex.match(title):
+            raise ValueError("The title must be composed only of alphanumeric characters and spaces.")
+        return title
+    
+    @validator("description")
+    def check_description_length(cls, description: str) -> str:
+        description_len = len(description)
+        if description_len > 200:
+            raise ValueError("The description must be at most 200 characters.")
+        return description
+    
+    @validator("color")
+    def check_color_format(cls, color: str) -> str:
+        if not cls._color_format_regex.match(color):
+            raise ValueError("The color should be a color hexadecimal string of length 6")
+        return color.upper()
---
+++ 1_siyad_final/user_model.py
@@ -0,0 +1,52 @@
+import re
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, HttpUrl, validator, Field
+
+
+class UserModel(BaseModel):
+    email: EmailStr
+    first_name: str = Field(alias = "firstName")
+    has_middle_name: bool = Field(alias = "hasMiddleName")
+    middle_name: Optional[str] = Field(alias = "middleName")
+    last_name: str = Field(alias = "lastName")
+    profile_url: HttpUrl = Field(alias = "profileUrl")
+    
+    _name_format_regex = re.compile(r"^[a-zA-Z]$")
+    
+    @validator("email", "first_name", "middle_name", "last_name", "profile_url")
+    def strip_spaces(cls, string: str) -> str:
+        return string.strip()
+    
+    @validator("middle_name")
+    def check_if_middle_name_should_be_included(cls, middle_name: Optional[str], values) -> Optional[str]:
+        if values["has_middle_name"] and middle_name is None:
+            raise ValueError("Checkbox picked for middle name but not middle name provided.")
+        elif not values["has_middle_name"] and middle_name is not None:
+            raise ValueError("Checkbox not picked for middle name but middle name provided")
+        return middle_name
+    
+    @validator("first_name", "middle_name", "last_name")
+    def check_name_part_len(cls, name_part: Optional[str]) -> Optional[str]:
+        if name_part is None:
+            return name_part
+        
+        name_part_len = len(name_part)
+        if name_part_len < 0 or name_part_len > 30:
+            raise ValueError("A name part must be between 1 and 30 characters in length")
+        return name_part
+    
+    @validator("first_name", "middle_name", "last_name")
+    def check_name_part_format(cls, name_part: Optional[str]) -> Optional[str]:
+        if name_part is None:
+            return name_part
+        if not cls._name_format_regex.match(name_part):
+            raise ValueError("A name part may only consist of alphabetical characters")
+        return name_part.capitalize()
+    
+    @validator("profile_url")
+    def check_profile_url_length(cls, profile_url: str) -> str:
+        profile_url_len = len(profile_url)
+        if profile_url_len < 13 or profile_url_len > 512:
+            raise ValueError("The profile URL length must be between 13 to 512 characters in length")
+        return profile_url
