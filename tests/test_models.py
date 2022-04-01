cd52b678641b018b21df5b539f70567e68687891
Nikunj Chawla <nikchawla312@gmail.com>
2022-04-01 01:25:26 -0500

    Added tests file

---
+++ tests/test_models.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+from pydantic.color import Color
+
+from p1_siyad_final import task_model as p1_task_model
+from p2_sumukh_final import task_model as p2_task_model
+from p3_aaron_final import task_model as p3_task_model
+
+correct_task_with_no_middle_name_with_field = {
+    "id": 1,
+    "title": "Hello Goodbye 7 l33t",
+    "description": "This task is obviously about hello goodbye 7 l33t. What more do you need?",
+    "dueDatetime": datetime(2022, 3, 31, 11, 32, 10),
+    "user": {
+        "email": "chawl025@umn.edu",
+        "firstName": "Nikunj",
+        "hasMiddleName": False,
+        "middleName": None,
+        "lastName": "Chawla",
+        "profileUrl": "https://github.com/nik312123"
+    },
+    "color": "#0197F6",
+    "priority": "low",
+    "isCompleted": False
+}
+
+
+def assert_model_dicts_equal(task_dict: dict, expected_dict: dict) -> None:
+    assert task_dict["id"] == expected_dict["id"]
+    assert task_dict["title"] == expected_dict["title"]
+    assert task_dict["description"] == expected_dict["description"]
+    assert task_dict["dueDatetime"] == expected_dict["dueDatetime"]
+    assert task_dict["user"]["email"] == expected_dict["user"]["email"]
+    assert task_dict["user"]["firstName"] == expected_dict["user"]["firstName"]
+    assert task_dict["user"]["middleName"] == expected_dict["user"]["middleName"]
+    assert task_dict["user"].get("hasMiddleName") == expected_dict["user"].get("hasMiddleName")
+    assert task_dict["user"]["lastName"] == expected_dict["user"]["lastName"]
+    assert task_dict["user"]["profileUrl"] == expected_dict["user"]["profileUrl"]
+    if isinstance(task_dict["color"], Color):
+        assert task_dict["color"].as_hex() == expected_dict["color"].as_hex()
+    else:
+        assert task_dict["color"] == expected_dict["color"]
+    assert task_dict["priority"] == expected_dict["priority"]
+    assert task_dict["isCompleted"] == expected_dict["isCompleted"]
+
+
+def test_correct_task_with_no_middle_name_with_field_p1() -> None:
+    task = p1_task_model.TaskModel(**correct_task_with_no_middle_name_with_field)
+    assert_model_dicts_equal(task.dict(by_alias = True), correct_task_with_no_middle_name_with_field)
+
+
+def test_correct_task_with_no_middle_name_with_field_p2() -> None:
+    task = p2_task_model.TaskModel(**correct_task_with_no_middle_name_with_field)
+    assert_model_dicts_equal(task.dict(by_alias = True), correct_task_with_no_middle_name_with_field)
+
+
+def test_correct_task_with_no_middle_name_with_field_p3() -> None:
+    task = p3_task_model.TaskModel(**correct_task_with_no_middle_name_with_field)
+    updated_dict = correct_task_with_no_middle_name_with_field.copy()
+    # noinspection PyTypeChecker
+    updated_dict["color"] = Color(updated_dict["color"])
+    updated_dict["user"].pop("hasMiddleName")
+    assert_model_dicts_equal(task.dict(by_alias = True), updated_dict)
