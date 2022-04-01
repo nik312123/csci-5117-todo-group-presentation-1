5dd2f214ea6358aaa53f13f911af76381fdf1c78
Nikunj Chawla <nikchawla312@gmail.com>
2022-03-31 21:47:11 -0500

    Expanded fname, mname, and lname to first_name, etc.

--- 3_aaron_final/user_model.py
+++ 3_aaron_final/user_model.py
@@ -7,10 +7,10 @@ from util import snake_case_to_camel_case
 
 class UserModel(BaseModel):
     email: EmailStr
-    fname: str
-    has_mname: bool
-    mname: Optional[str]
-    lname: str
+    first_name: str
+    has_middle_name: bool
+    middle_name: Optional[str]
+    last_name: str
     profile_url: Optional[HttpUrl]
     
     class Config:
@@ -20,20 +20,20 @@ class UserModel(BaseModel):
         min_anystr_length = 1
         max_anystr_length = 30
         fields = {
-            "has_mname": {
+            "has_middle_name": {
                 "exclude": True
             }
         }
     
-    @validator("mname")
-    def check_if_mname_should_be_included(cls, mname: Optional[str], values) -> Optional[str]:
-        if values["has_mname"] and mname is None:
+    @validator("middle_name")
+    def check_if_middle_name_should_be_included(cls, middle_name: Optional[str], values) -> Optional[str]:
+        if values["has_middle_name"] and middle_name is None:
             raise ValueError("Checkbox picked for middle name but not middle name provided.")
-        elif not values["has_mname"] and mname is not None:
+        elif not values["has_middle_name"] and middle_name is not None:
             raise ValueError("Checkbox not picked for middle name but middle name provided")
-        return mname
+        return middle_name
     
-    @validator("fname", "mname", "lname")
+    @validator("first_name", "middle_name", "last_name")
     def replace_spaces_in_name_part_with_underscores(cls, name_part: str) -> str:
         return name_part.replace(" ", "_")
     
